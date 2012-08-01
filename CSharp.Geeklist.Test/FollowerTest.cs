9432d90f40bf34847da7bdf7bc547ac005068f2f
coolya <k.dummann@gmail.com>
2012-08-01 08:28:44 +0200

    refactor unittest to match the new api

--- CSharp.Geeklist.Test/FollowerTest.cs
+++ CSharp.Geeklist.Test/FollowerTest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSharp.Geeklist.Api.Interfaces;
+using CSharp.Geeklist.Connect;
+using Chq.OAuth;
+using Chq.OAuth.Credentials;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
 namespace CSharp.Geeklist.Test
@@ -11,17 +14,48 @@ namespace CSharp.Geeklist.Test
     [TestClass]
     public class FollowerTest
     {
-        static CSharp.Geeklist.Api.Interfaces.IFollowerOperations _ops = new Geeklist.Connect
-                .GeeklistServiceProvider("6S8YzYWymDGfb6mB8VPR5Wj_03M", "sz0Wp8bzq_U_Fgsum7XBJrUWSIuIBCvIpoR8PMpMCts")
-                .GetApi("mXTwZHawIxlOTf-rP9WpplrFERE", "FiVyVFRIrAILzkZ2TTflQu1uNrMJKnREISS1NiwBzEA").FollowerOperations;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var context = new OAuthContext("6S8YzYWymDGfb6mB8VPR5Wj_03M", "sz0Wp8bzq_U_Fgsum7XBJrUWSIuIBCvIpoR8PMpMCts",
+                                          GeeklistServiceProvider.RequestTokenUri,
+                                          GeeklistServiceProvider.AuthorizeUri,
+                                          GeeklistServiceProvider.AccessTokenUri);
+
+                var client = new Client(context);
+
+                var requestTokenResponse = client.MakeRequest("GET")
+                .ForRequestToken()
+                .Sign()
+                .ExecuteRequest().Result;
+
+                client.RequestToken = TokenContainer.Parse(requestTokenResponse);
+
+                client.AccessToken = new TokenContainer() { Token = "mXTwZHawIxlOTf-rP9WpplrFERE", Secret = "FiVyVFRIrAILzkZ2TTflQu1uNrMJKnREISS1NiwBzEA" };
+
+                _provider = new GeeklistServiceProvider(uri => client.MakeRequest("GET").ForResource(client.AccessToken.Token, uri).Sign(client.AccessToken.Secret).ExecuteRequest().AsAsyncOperation(),
+                                                        (uri, o) => client.MakeRequest("GET").ForResource(client.AccessToken.Token, uri).WithParameters(o).Sign(client.AccessToken.Secret).ExecuteRequest().AsAsyncOperation(),
+                                                        uri => client.MakeRequest("POST").ForResource(client.AccessToken.Token, uri).Sign(client.AccessToken.Secret).ExecuteRequest().AsAsyncOperation(),
+                                                        (uri, o) => client.MakeRequest("POST") .ForResource(client.AccessToken.Token, uri).WithFormEncodedData(o).Sign(client.AccessToken.Secret).ExecuteRequest().AsAsyncOperation());
+        }
+        private GeeklistServiceProvider _provider;
+
+
+        private IFollowerOperations _ops
+        {
+            get
+            {
+                 return  _provider.GetApi().FollowerOperations;
+            }
+        }
+
 
         private IUserOperations UserOps
         {
             get
             {
-                return new Geeklist.Connect
-                    .GeeklistServiceProvider("6S8YzYWymDGfb6mB8VPR5Wj_03M", "sz0Wp8bzq_U_Fgsum7XBJrUWSIuIBCvIpoR8PMpMCts")
-                    .GetApi("mXTwZHawIxlOTf-rP9WpplrFERE", "FiVyVFRIrAILzkZ2TTflQu1uNrMJKnREISS1NiwBzEA").UserOperations;
+                 return  _provider.GetApi().UserOperations;
             }
         }
 
